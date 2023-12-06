4548420cabdc4c9f511a5472146c1ed4543e0806
antoponselvan <anto.ponselvan@gmail.com>
2023-12-06 23:22:23 +0800

    After lodash

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as _ from 'lodash'
+
+export const handler = async () => {
+  const max = 10
+  const val = _.random(max)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`Wassup from lambda! - Random# : ${val}`)
+  }
+  return response
+}
